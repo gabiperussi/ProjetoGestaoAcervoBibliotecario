39d7055d28877db6d2d43a2a3de6eeefc369295a
GABRIELLY PERUSSI CORRÊA <gabrielly.p.correa6@aluno.senai.br>
2026-02-25 12:04:54 -0300

    primeira versao

--- api_livros/config/testedb.php
+++ api_livros/config/testedb.php
@@ -3,13 +3,23 @@ require_once 'bd.php';
 
 try {   
     $database = new Database();
-    
     $db = $database->getConnection(); 
 
     if ($db) {
-        echo "Sucesso: A ponte com o banco de dados funcionou";
+        $stmt = $db->query("SELECT id_livro, titulo, autor FROM livros2");
+
+        
+        $livros = []; 
+
+       
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $livros[] = $row; 
+        }
+
+        
+        echo json_encode($livros, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT); 
     }
 } catch (Exception $e) {
-    echo "Erro inesperado: " . $e->getMessage();
+    echo json_encode(["erro" => $e->getMessage()]);
 }
 ?>
